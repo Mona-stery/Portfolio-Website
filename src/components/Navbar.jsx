9e2f996e4cfea5a0c6ff0f0347abf4f6d5296ea4
Mona-stery <aitchesonmaximilian@gmail.com>
2025-01-31 00:58:27 +0100

    refactor Navbar component for improved readability and consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,13 @@
-import logo from '../assets/KevinRushLogo.png'
-import { FaLinkedin } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
-import { FaTwitter } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
+import logo from "../assets/KevinRushLogo.png";
+import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Navbar = () => {
     return ( 
     <nav className="mb-20 flex items-center justify-between py-6">
-      <div className="flex flex-shrink-0 item-center">
+      <div className="flex flex-shrink-0 items-center">
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
-      <div className="m-8 flex item-center justify-center gap-4 text-2xl">
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
         <FaLinkedin />
         <FaGithub />
         <FaTwitter />
