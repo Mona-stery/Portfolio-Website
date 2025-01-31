7029090ab7535a88349ed135f72847948de486c2
Mona-stery <aitchesonmaximilian@gmail.com>
2025-01-31 17:54:13 +0100

    update Navbar component to link social media icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
-import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaTwitter, } from 'react-icons/fa';
 
 const Navbar = () => {
     return ( 
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaTwitter />
-        <FaInstagram />
+        <a href="https://www.linkedin.com/in/maximilian-aitcheson-70b15b310/"><FaLinkedin /></a>
+        <a href="https://github.com/Mona-stery"><FaGithub /></a>
+        <a href="https://x.com/maxaitchesondev"><FaTwitter /></a>
       </div>
     </nav>
   );
