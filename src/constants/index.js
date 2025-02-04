3c55907c8acae591b720ae57992d1fcc0881f566
Mona-stery <aitchesonmaximilian@gmail.com>
2025-02-04 16:57:15 +0100

    update project image format from JPG to PNG for project 1

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import project1 from "../assets/projects/project-1.jpg";
+import project1 from "../assets/projects/project-1.png";
 import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.jpg";
 
